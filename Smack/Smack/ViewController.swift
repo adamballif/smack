6996a0bd34b3847443127986f802e177e7ee3665
Adam Ballif <adamballif@Adams-15-MacBook-Pro.local>
2019-02-07 16:01:26 -0700

    initial xcode project

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Adam Ballif on 2/7/19.
+//  Copyright © 2019 Adam Ballif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
